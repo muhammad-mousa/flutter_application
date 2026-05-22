906a03dc39b9eae78af1be1e0e0ffe9167490548
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-05-22 18:58:34 +0500

    added material theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,62 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 141, 99, 212),
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Wrap(
-        children: [
-          Text(
-            'This is the Text..This is the Text..This is the Text..This is the Text..This is the Text..This is the Text..This is the Text..This is the Text..',
-          ),
-          Text(
-            'This is the Text..This is the Text..This is the Text..This is the Text..This is the Text..This is the Text..',
-          ),
-          Text('This is the Text..'),
-          Text('This is the Text..'),
-          Text('This is the Text..'),
-          Text('This is the Text..'),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter App')),
+        body: const Center(child: Text('Hello, World!')),
       ),
     );
   }
