cca081212fff28e7165bcda53658806b91775f37
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-05-25 11:41:42 +0500

    added the floating action button and drawer with Safe Area

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,40 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Mapp'),
-          centerTitle: true,
-          backgroundColor: Colors.teal,
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: const [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-          ],
-          onDestinationSelected: (int index) {
-            print('Selected index: $index');
-          },
-          selectedIndex: 0,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: const Text('Flutter Mapp'), centerTitle: true),
+          drawer: Drawer(
+            child: Column(children: [ListTile(title: const Text('Logout'))]),
+          ),
+          floatingActionButton: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  print('Floating Action Button Pressed');
+                },
+                child: const Icon(Icons.add),
+              ),
+              SizedBox(height: 10), // Add some spacing between the buttons
+              FloatingActionButton(
+                onPressed: () {
+                  print('Second Floating Action Button Pressed');
+                },
+                child: const Icon(Icons.edit),
+              ),
+            ],
+          ),
+          bottomNavigationBar: NavigationBar(
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+            ],
+            onDestinationSelected: (int index) {
+              print('Selected index: $index');
+            },
+            selectedIndex: 0,
+          ),
         ),
       ),
     );
