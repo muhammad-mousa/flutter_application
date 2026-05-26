34ef87e5e6634f12887a229204c3436f0527c2ac
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-05-26 15:12:05 +0500

    added hero widget and logout button in profile

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home Page', style: TextStyle(fontSize: 24)));
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(children: [HeroWidget()]),
+    );
   }
 }
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/views/widget_tree.dart';
+import 'package:flutter_application/views/widgets/hero_widget.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -12,10 +13,7 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Image.asset('assets/images/bg.jpg'),
-            ),
+            HeroWidget(),
             FilledButton(
               onPressed: () {
                 Navigator.pushReplacement(
