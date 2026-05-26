f47c5819969cfcbd8d78f335a848f134e4e7cdb7
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-05-26 17:40:25 +0500

    added lotties in the project for the welcome page

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/views/widget_tree.dart';
-import 'package:flutter_application/views/widgets/hero_widget.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -13,7 +13,7 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            HeroWidget(),
+            Lottie.asset('assests/lotties/welcome.json'),
             FilledButton(
               onPressed: () {
                 Navigator.pushReplacement(
