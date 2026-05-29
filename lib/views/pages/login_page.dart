b77f10a946321c17c9e012833f995d4d4b454e4d
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-05-29 17:11:29 +0500

    add the push and remove to remove the last pages

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -90,13 +90,14 @@ class _LoginPageState extends State<LoginPage> {
   void onLoginPressed() {
     if (controllerEmail.text == confirmedEmail &&
         controllerPassword.text == confirmedPassword) {
-      Navigator.pushReplacement(
+      Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(
           builder: (context) {
             return WidgetTree();
           },
         ),
+        (route) => false,
       );
     } else {
       ScaffoldMessenger.of(
