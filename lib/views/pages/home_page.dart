a9ae9b9b31a8aa989df7aa429a22cb045100967e
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-06-01 13:45:19 +0500

    update the app icon launcher using the flutter_launcher icon pkg and update the home page make the hero widget clickable using gesture detector and added course page

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/data/constants.dart';
+import 'package:flutter_application/views/pages/course_page.dart';
 import 'package:flutter_application/views/widgets/container_widget.dart';
 import 'package:flutter_application/views/widgets/hero_widget.dart';
 
@@ -7,27 +9,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> list = [
+      KValue.basicLayout,
+      KValue.keyConcepts,
+      KValue.cleanUi,
+      KValue.fixBugs,
+    ];
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: SingleChildScrollView(
         child: Column(
           children: [
-            HeroWidget(title: 'Flutter  Mapp'),
-            ContainerWidget(
-              title: 'Basic Layout',
-              description:
-                  'You can add more widgets here to create a more complex card design.',
-            ),
-            ContainerWidget(
-              title: 'Basic Layout',
-              description:
-                  'You can add more widgets here to create a more complex card design.',
-            ),
-            ContainerWidget(
-              title: 'Basic Layout',
-              description:
-                  'You can add more widgets here to create a more complex card design.',
-            ),
+            HeroWidget(title: 'Flutter  Mapp', nextPage: CoursePage()),
+            ...List.generate(list.length, (index) {
+              return ContainerWidget(
+                title: list.elementAt(index),
+                description: 'This is a description',
+              );
+            }),
           ],
         ),
       ),
--- lib/views/widgets/hero_widget.dart
+++ lib/views/widgets/hero_widget.dart
@@ -1,37 +1,52 @@
 import 'package:flutter/material.dart';
 
 class HeroWidget extends StatelessWidget {
-  const HeroWidget({super.key, required this.title});
+  const HeroWidget({super.key, required this.title, this.nextPage});
 
+  final Widget? nextPage;
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Hero(
-          tag: 'hero',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image.asset(
-              'assets/images/bg.jpg',
-              color: Colors.teal,
-              colorBlendMode: BlendMode.darken,
+    return GestureDetector(
+      onTap: nextPage != null
+          ? () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return nextPage!;
+                  },
+                ),
+              );
+            }
+          : null,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Hero(
+            tag: 'hero',
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image.asset(
+                'assets/images/logo.png',
+                color: Colors.teal,
+                colorBlendMode: BlendMode.darken,
+              ),
             ),
           ),
-        ),
-        FittedBox(
-          child: Text(
-            title,
-            style: TextStyle(
-              fontSize: 50.0,
-              letterSpacing: 50.0,
-              color: Colors.white30,
+          FittedBox(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 50.0,
+                letterSpacing: 50.0,
+                color: Colors.white30,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
