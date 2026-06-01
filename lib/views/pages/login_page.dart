e9d1ae7a4eb3b0e6bfba54418e1c44619ecc1fd1
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-06-01 15:52:42 +0500

    added fractionally sized box and layout bilder for the responsive ui

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -40,46 +40,53 @@ class _LoginPageState extends State<LoginPage> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Lottie.asset('assets/lotties/home.json', height: 400.0),
-                TextField(
-                  controller: controllerEmail,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
+            child: LayoutBuilder(
+              builder: (context, BoxConstraints constraints) {
+                return FractionallySizedBox(
+                  widthFactor: constraints.maxWidth > 500 ? 0.5 : 1.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset('assets/lotties/home.json', height: 400.0),
+                      TextField(
+                        controller: controllerEmail,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onEditingComplete: () {
+                          setState(() {});
+                          print('Hello ');
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: controllerPassword,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 15),
+                      FilledButton(
+                        onPressed: () {
+                          onLoginPressed();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 50.0),
+                        ),
+                        child: Text(widget.title),
+                      ),
+                      SizedBox(height: 50),
+                    ],
                   ),
-                  onEditingComplete: () {
-                    setState(() {});
-                    print('Hello ');
-                  },
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: controllerPassword,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                  ),
-                  obscureText: true,
-                ),
-                SizedBox(height: 15),
-                FilledButton(
-                  onPressed: () {
-                    onLoginPressed();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(double.infinity, 50.0),
-                  ),
-                  child: Text(widget.title),
-                ),
-                SizedBox(height: 50),
-              ],
+                );
+              },
             ),
           ),
         ),
