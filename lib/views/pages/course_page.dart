f4855b5f0028ef29a67cba3985fc48adf0932964
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-06-02 11:09:05 +0500

    added http pkg and make a get call and parse it into activity class and added the permissions for the prod to make call from the internet

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/data/classes/activity_class.dart';
 import 'package:flutter_application/views/widgets/hero_widget.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  late Activity activity;
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      activity = Activity.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>,
+      );
+      print(activity.activity);
+    } else {
+      throw Exception('Failed to load activity');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
