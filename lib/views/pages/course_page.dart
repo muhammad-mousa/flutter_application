3c1155af4e07c3fdd24d1b119d0de18c6cd7f338
Mousa Naeem <mousa.naeem.01@outlook.com>
2026-06-02 11:21:30 +0500

    show api get data in the screen visuals

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -12,23 +12,20 @@ class CoursePage extends StatefulWidget {
 }
 
 class _CoursePageState extends State<CoursePage> {
-  late Activity activity;
-
   @override
   void initState() {
     getData();
     super.initState();
   }
 
-  void getData() async {
+  Future getData() async {
     var url = Uri.https('bored-api.appbrewery.com', '/random');
 
     var response = await http.get(url);
     if (response.statusCode == 200) {
-      activity = Activity.fromJson(
+      return Activity.fromJson(
         jsonDecode(response.body) as Map<String, dynamic>,
       );
-      print(activity.activity);
     } else {
       throw Exception('Failed to load activity');
     }
@@ -38,11 +35,31 @@ class _CoursePageState extends State<CoursePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: SingleChildScrollView(
-          child: Column(children: [HeroWidget(title: 'Course')]),
-        ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          Widget widget;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            widget = Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            widget = Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroWidget(title: activity.activity),
+                    Text(activity.activity, style: TextStyle(fontSize: 20.0)),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            widget = const Center(child: Text('error!'));
+          }
+          return widget;
+        },
       ),
     );
   }
